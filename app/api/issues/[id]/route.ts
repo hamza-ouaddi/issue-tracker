0e60d6c81f396f75fb46fecb68ae156faa0c11d9
hamza-ouaddi <hamza0addi@gmail.com>
2024-01-14 01:11:43 +0100

    implement edit issue functionality

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,33 @@
+import { IssueSchema } from "@/lib/validations";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const validation = IssueSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  }
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue) {
+    return NextResponse.json({ error: "Issue not found" }, { status: 404 });
+  }
+
+  const updateIssue = await prisma.issue.update({
+    where: { id: issue.id },
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(updateIssue);
+}
