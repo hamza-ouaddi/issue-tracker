bedb0ec9776948dda0f249f9ee0a8992e3e5c69d
hamza-ouaddi <hamza0addi@gmail.com>
2024-01-16 20:29:48 +0100

    implement user authentication middleware for action restrictions and endpoints protection

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 
 const issueSchema = z.object({
   title: z.string().min(1).max(255),
@@ -8,6 +10,12 @@ const issueSchema = z.object({
 });
 
 export async function POST(request: NextRequest) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({}, { status: 401 });
+  }
+
   const body = await request.json();
   const validation = issueSchema.safeParse(body);
 
