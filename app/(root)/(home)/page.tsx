6f67c12512cb12fa65a920301d7317535faf6740
hamza-ouaddi <hamza0addi@gmail.com>
2024-01-24 17:49:26 +0100

    add latest issues component to dashboard page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,13 @@
+import LatestIssues from "@/components/LatestIssues";
 import React from "react";
 
 export default async function Home() {
   return (
     <div>
       <h1 className="h1-bold text-primary900_light900">Dashboard</h1>
+      <div className="mt-16">
+        <LatestIssues />
+      </div>
     </div>
   );
 }
